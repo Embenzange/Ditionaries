b83031c555e56b99537e0d04d3e2f980b0ebfe9f
Bertha <kasiera4882@outlook.com>
2024-01-09 17:31:00 +0300

    C1

---
+++ dictionaries.py
@@ -0,0 +1,9 @@
+Chief_Maverick = {
+    'Name' : 'Bertha Embenzange',
+    'Age' : '27',
+    'Company' : 'NannyLinked',
+}
+
+print(Chief_Maverick)
+print(Chief_Maverick['Company'])
+print(Chief_Maverick['Age'])
