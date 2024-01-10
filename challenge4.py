f836f4afceda68855e8fb112f0858bec0daa482e
Embenzange <berthakasiera@gmail.com>
2024-01-10 17:51:17 +0300

    C1

--- challenge4.py
+++ challenge4.py
@@ -3,6 +3,6 @@ def total_price(original_price, discount_percentage):
     return(grand_total)
 
 original_price = float(input("What is your total: "))
-discount = 20/100 * original_price
+discount = 0.2 * original_price
 grand_total = original_price - discount
 print("Thank you for shopping GasMall. Your grand total after the discount is: ", grand_total)
