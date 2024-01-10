290d99df9dd1b01222662af59f2df5ad1abde347
Embenzange <berthakasiera@gmail.com>
2024-01-10 17:49:32 +0300

    Commit 4

---
+++ challenge4.py
@@ -0,0 +1,8 @@
+#Create a function that takes two arguments: the original price and the discount percentage as integers and returns the final price after the discount.
+def total_price(original_price, discount_percentage):
+    return(grand_total)
+
+original_price = float(input("What is your total: "))
+discount = 20/100 * original_price
+grand_total = original_price - discount
+print("Thank you for shopping GasMall. Your grand total after the discount is: ", grand_total)
